c6832027debc864df628560070620f853421b0a9
NguyenHuuMinh <nguyenhuuminh8204@gmail.com>
2024-11-26 00:11:40 +0700

    complete models function
    Not tested

--- models/users.js
+++ models/users.js
@@ -3,28 +3,43 @@ import { collection, doc, getDoc, getDocs, updateDoc, setDoc } from 'firebase/fi
 
 const userData = {
     all: async () => {
-        const querySnapshot = await getDocs(collection(db, 'user'));
-        return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    },
-
-    one: async (id) => {
-        const userRef = doc(db, 'user', id);
-        const docSnap = await getDoc(userRef);
-        return docSnap.exists() ? { id: docSnap.id, ...docSnap.data() } : null;
-    },
-
-    update: async (id, newUpdate) => {
-        const userRef = doc(db, 'user', id);
-        await updateDoc(userRef, newUpdate);
-        return { status: true };
-    },
-
-    add: async (user) => {
         try {
-            const userRef = doc(collection(db, 'user'), user.uid);
+            const querySnapshot = await getDocs(collection(db, 'user'));
+            return querySnapshot.docs.map(doc => ({ uid: doc.id, ...doc.data() }));
+        } catch (e) {
+            console.error("Error fetching users: ", e);
+            throw new Error('Failed to fetch users');
+        }
+    },
+
+    get: async (uid) => {
+        try {
+            const userRef = doc(db, 'user', uid);
+            const docSnap = await getDoc(userRef);
+            return docSnap.exists() ? { uid: docSnap.id, ...docSnap.data() } : null;
+        } catch (e) {
+            console.error(`Error fetching user with ID ${uid}:`, e);
+            throw new Error('Failed to fetch user');
+        }
+    },
+
+    update: async (uid, newUpdate) => {
+        try {
+            const userRef = doc(db, 'user', uid);
+            await updateDoc(userRef, newUpdate);
+            return { status: true };
+        } catch (e) {
+            console.error(`Error updating user with ID ${uid}:`, e);
+            return { status: false, error: e.message };
+        }
+    },
+
+    new: async (uid, user) => {
+        try {
+            const userRef = doc(collection(db, 'user'), uid);
             await setDoc(userRef, user);
 
-            return { status: true, id: userRef.id };
+            return { status: true, uid: userRef.id };
         } catch (e) {
             console.error("Error adding document: ", e);
             return { status: false, error: e.message };
